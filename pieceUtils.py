a619337497f51b4198e62e465cc45d52fdcd6a10
engineerdragon01 <dragonboy174@gmail.com>
2024-07-13 13:16:01 -0700

    [Debugging] Trying to correct height offset method conditional

--- pieceUtils.py
+++ pieceUtils.py
@@ -1,20 +1,44 @@
+import array as arr
 #
 class PieceUtils:
     
     def __init__(self):
-        # Initialize Dictionary with Key: pieceLetter -> Value: Elementary coords of piece's four blocks (from left to right, so bottom  pieces first)
-        letterMap = {}
-        letterMap.update({"Q": [(0,0),(0,0),(0,0),(0,0)]})
-        letterMap.update({"Z": [(0,0),(0,0),(0,0),(0,0)]})
-        letterMap.update({"S": [(0,0),(0,0),(0,0),(0,0)]})
-        letterMap.update({"T": [(0,0),(0,0),(0,0),(0,0)]})
-        letterMap.update({"I": [(0,0),(0,0),(0,0),(0,0)]})
-        letterMap.update({"L": [(0,0),(0,0),(0,0),(0,0)]})
-        letterMap.update({"J": [(0,0),(0,0),(0,0),(0,0)]})
-        self.pieces = letterMap
+        # Key: pieceLetter -> Value: Elementary coords Piece's 4 blocks (based on (0,0) origin)
+        coordMap = {}
+        coordMap.update({"Q": [[0,0],[0,1],[1,0],[1,1]]})
+        coordMap.update({"Z": [[0,1],[1,0],[1,1],[2,0]]})
+        coordMap.update({"S": [[0,0],[1,0],[1,1],[2,1]]})
+        coordMap.update({"T": [[0,1],[1,0],[1,1],[2,1]]})
+        coordMap.update({"I": [[0,0],[1,0],[2,0],[3,0]]})
+        coordMap.update({"L": [[0,0],[1,0],[0,1],[0,2]]})
+        coordMap.update({"J": [[0,0],[1,0],[1,1],[1,2]]})
+        self.pieceCoords = coordMap
+        # Key: pieceLetter -> Value: height
+        heightsMap = {}
+        heightsMap.update({"Q":2})
+        heightsMap.update({"Z":2})
+        heightsMap.update({"S":2})
+        heightsMap.update({"T":2})
+        heightsMap.update({"I":1})
+        heightsMap.update({"L":3})
+        heightsMap.update({"J":3})
+        self.pieceHeights = heightsMap
+        # Key: pieceLetter -> Value: width
+        widthsMap = {}
+        widthsMap.update({"Q":2})
+        widthsMap.update({"Z":3})
+        widthsMap.update({"S":3})
+        widthsMap.update({"T":3})
+        widthsMap.update({"I":4})
+        widthsMap.update({"L":2})
+        widthsMap.update({"J":2})
+        self.pieceWidths = widthsMap
 
-    def getPieceOrientation(pieceLetter):
-        pass
+    def getPieceCoords(self, pieceLetter):
+        return self.pieceCoords[pieceLetter]
 
-    def getPieceHeight(pieceLetter):
-        pass
+    def getPieceWidth(self, pieceLetter):
+        return self.pieceHeights[pieceLetter]
+
+    def getPieceHeight(self, pieceLetter):
+        return self.pieceWidths[pieceLetter]
--- tetris.py
+++ tetris.py
@@ -1,15 +1,15 @@
 from pieceUtils import PieceUtils
 from gridUtils import GridUtils
 import sys
+import copy
 
 # Read STDIN and Parse Each Testcase String into Separate Lists
 raw_game = sys.stdin.readlines()
 games = []
-for game in games:
+for game in raw_game:
     game = game.strip()
     game = game.split(",")
     games.append(game)
-    print(game)
 
 # Instantiate Grid Representation
 gridUtils = GridUtils()
@@ -17,13 +17,52 @@ gridUtils = GridUtils()
 # Instantiate Piece Representation
 pieceUtils = PieceUtils()
 
+# Solution List
+solutions = []
+
 # Execute Tetris Game
 for seq in games:
     # Play Testcase
     for move in seq:
+        print("Move: ", move)
+        # Determine Piece and Column
+        movePiece = move[0]
+        moveCol = int(move[1])
+
+        moveCoords = pieceUtils.getPieceCoords(movePiece)
+        print("Move Coordinates: ", moveCoords)
         # Place Piece in Grid
-        pass
+        gridUtils.placePiece(moveCol, movePiece, moveCoords, pieceUtils)
+        print("Grid After Move: ")
+        for row in gridUtils.getGridObject():
+            print(row)
+        print("MaxHeight before shift: ", max(gridUtils.getColMaxHeights()))
+
+        # Scan grid for full rows and shift rows down accordingly
+        fullRowIdx = 0
+        while fullRowIdx >= 0:
+            print("Grid Before Full Row Search: ")
+            for row in gridUtils.getGridObject():
+                print(row)
+            fullRowIdx = gridUtils.findFullRow()
+            print("Full Row Idx: ", fullRowIdx)
+        # Adjust rows based on full row removal
+            if fullRowIdx >= 0:
+                gridUtils.shiftRows(fullRowIdx)
+        # Update maxHeightOfCols list
+        # gridUtils.updateColMaxHeights()
+        print("----")
+    print("|||||| Solution for this case: ", max(gridUtils.getColMaxHeights()))
+    print("-------------------")
+    # Save max height of testcase to solutions
+    solutions.append(max(gridUtils.getColMaxHeights()))
+    # Clear the Grid After Each TestCase
+    gridUtils.clearGrid()
 
 # Write Solution to STDOUT
 # We Want to Return the Tallest Height of each Testcase
 # Use sys.stdout.write() or can try seeing what print does since that outputs to STDOUT
+for sol in solutions:
+    sys.stdout.write(sol)
+# print(solutions)
+# print(gridUtils.getColMaxHeights())
